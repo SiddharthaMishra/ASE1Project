e5ea270ccdf8e5b5b3b358563febcd16a24f7c17
Swathi-Sindhu <37298098+Swathi-Sindhu@users.noreply.github.com>
2018-11-18 23:52:47 +0530

    regist

---
+++ login_signup/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from login_signup import views
+from django.urls import path
+#from django.contrib import auth
+
+app_name = 'login_signup'
+urlpatterns=[
+    path('',views.signup),
+    url(r'^signup/$',views.signup,name='signup'),
+    url(r'^user_login/$',views.user_login,name='user_login'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate , name='activate'),
+
+]
