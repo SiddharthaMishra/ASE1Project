d253dce4acc451950ca050a5756606b7f4a8ff2f
Rupesh <somanarupesh32@gmail.com>
2018-12-12 23:13:31 +0530

    Added Contact Us Page

--- securestore/static/scripts123.js
+++ securestore/static/scripts123.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 			a=b.toLowerCase();
 		
 			
-			if(a.includes('hi')==true){
+			if(a.includes('hi')==true ||a.includes('hai')==true|| a.includes('hello')==true){
 				var w='Hi user,how can i help u'
 				$('<div class="msg-left">'+w+'</div>').insertBefore('[rel="'+chatbox+'"] .msg_push');
 				$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
@@ -57,14 +57,24 @@ $(document).ready(function(){
 				$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
 				$("#login")[0].click();
 			}
-			if(a.includes('charge')==true){
+			if(a.includes('securestore')==true){
+				
+				var w="SECURE STORE is our website,you can stores ur files securely";
+				
+
+				$('<div class="msg-left" id="login" on>'+w+'</div>').insertBefore('[rel="'+chatbox+'"] .msg_push');
+
+				$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
+				$("#login")[0].click();
+			}
+			if(a.includes('charge')==true||a.includes('price')==true||a.includes('money')==true||a.includes('money')==true){
 				var w='U will be given 15GB data ,for more u need to pay'
 				$('<div class="msg-left">'+w+'</div>').insertBefore('[rel="'+chatbox+'"] .msg_push');
 				$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
 			}
 			
-		   if(a.includes('hi')==false && a.includes('signup')==false && a.includes('join')==false && a.includes('login')==false && a.includes('charge')==false){
-				var w='Sorry we couldnt understand, We will come back to u see soon'
+		   if(a.includes('price')==false&&a.includes('money')==false&&a.includes('money')==false &&a.includes('hai')==false&& a.includes('hello')==false && a.includes('hi')==false && a.includes('signup')==false && a.includes('join')==false && a.includes('login')==false && a.includes('charge')==false){
+				var w='Sorry we couldnt understand, We will come back to u soon'
 				$('<div class="msg-left">'+w+'</div>').insertBefore('[rel="'+chatbox+'"] .msg_push');
 				$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
 			   }
